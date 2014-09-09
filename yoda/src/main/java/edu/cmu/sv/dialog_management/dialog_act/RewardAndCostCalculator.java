1617ba5dece21b3dd3c71852f8b39acaf3a60f40
David Cohen <david.cohen@sv.cmu.edu>
2014-09-09 15:01:45 -0700

    all three initial reward calculation functions are written. I still need to re-write the supporting confidence prediction functions to return expected relative improvements per hypothesis instead of the expected top joint

--- yoda/src/main/java/edu/cmu/sv/dialog_management/dialog_act/RewardAndCostCalculator.java
+++ yoda/src/main/java/edu/cmu/sv/dialog_management/dialog_act/RewardAndCostCalculator.java
@@ -4,6 +4,9 @@ import edu.cmu.sv.dialog_management.DialogRegistry;
 import edu.cmu.sv.dialog_management.DiscourseUnit;
 import edu.cmu.sv.semantics.SemanticsModel;
 import edu.cmu.sv.task_interface.dialog_task.DialogTask;
+import edu.cmu.sv.task_interface.dialog_task.DialogTaskPreferences;
+import edu.cmu.sv.task_interface.non_dialog_task.NonDialogTask;
+import edu.cmu.sv.task_interface.non_dialog_task.NonDialogTaskPreferences;
 import edu.cmu.sv.utils.StringDistribution;
 
 import java.util.Set;
@@ -16,7 +19,24 @@ import java.util.Set;
  */
 public class RewardAndCostCalculator {
 
-    public static Double nonDialogTaskReward(DiscourseUnit DU){return null;}
+    public static Double nonDialogTaskReward(DiscourseUnit DU, NonDialogTask nonDialogTask){
+        Double totalReward = 0.0;
+        Double probabilityCorrect = 0.0;
+        for (String key : DU.getHypotheses().keySet()){
+            if (nonDialogTask.meetsTaskSpec(DU.getHypotheses().get(key)))
+                probabilityCorrect += DU.getHypothesisDistribution().get(key);
+        }
+        // multiply probability correct by probability that the task is executable
+        probabilityCorrect *= nonDialogTask.assessExecutability();
+
+        // we add the penalty for delay to the reward, because that is equivalent to subtracting it
+        // from all other actions under consideration
+        totalReward += probabilityCorrect * (nonDialogTask.getPreferences().rewardForCorrectExecution +
+                nonDialogTask.getPreferences().penaltyForDelay);
+        totalReward -= (1.0-probabilityCorrect) * nonDialogTask.getPreferences().penaltyForIncorrectExecution;
+
+        return totalReward;
+    }
 
     public static Double dialogTaskReward(DiscourseUnit DU, DialogTask dialogTask){
         Double totalReward = 0.0;
@@ -37,43 +57,53 @@ public class RewardAndCostCalculator {
         return totalReward;
     }
 
-    public static Double slotFillingDialogActReward(DiscourseUnit DU){return null;}
-
     /*
     * To compute the reward for a clarification dialog act,
-    * incorporate all the dialog task rewards and non dialog task rewards,
-    * weighed by the presence of relevant dialog acts in the DU hypotheses n-best list
-    *
+    * estimate the improvement in reward to all possible dialog and non-dialog tasks
+    * that relate to all the available DU hypotheses.
     * */
-    public static Double clarificationDialogActReward(DiscourseUnit DU, Double predictedConfidence)
+    public static Double clarificationDialogActReward(DiscourseUnit DU, Double predictedRelativeConfidenceGain)
             throws IllegalAccessException, InstantiationException {
         Double totalReward = 0.0;
-        StringDistribution predictedDistribution = DU.getHypothesisDistribution().deepCopy();
-        predictedDistribution.setAndNormalize(predictedDistribution.getTopHypothesis(), predictedConfidence);
 
+        // sum up the predicted rewards supposing that each current hypothesis is true,
+        // weighting the predicted reward by the current belief that the hypothesis is true.
         for (String hypothesisID : DU.getHypotheses().keySet()){
             SemanticsModel hypothesis = DU.getHypotheses().get(hypothesisID);
+            Double currentConfidence = DU.getHypothesisDistribution().get(hypothesisID);
+            Double predictedConfidence = currentConfidence + (1-currentConfidence)*predictedRelativeConfidenceGain;
+
+            // predict the difference in expected reward after clarification
+            Double predictedRewardDifference = 0.0;
             Class<? extends DialogAct> daClass = DialogRegistry.dialogActNameMap.
                     get(hypothesis.getSlotPathFiller("dialogAct"));
+            // add contribution from dialog tasks
             for (Class<? extends DialogTask> taskClass : DialogRegistry.dialogTaskRegistry.get(daClass)){
-                DialogTask dialogTask = taskClass.newInstance();
-                dialogTask.setTaskSpec(hypothesis.deepCopy());
-                Double probabilityCorrect = 
+                DialogTaskPreferences preferences = taskClass.newInstance().getPreferences();
+                predictedRewardDifference += predictedConfidence * preferences.rewardForCorrectExecution;
+                predictedRewardDifference -= (1 - predictedConfidence) * preferences.penaltyForIncorrectExecution;
+                predictedRewardDifference -= currentConfidence * preferences.rewardForCorrectExecution;
+                predictedRewardDifference += (1 - currentConfidence) * preferences.penaltyForIncorrectExecution;
+                totalReward += currentConfidence * predictedRewardDifference /
+                        DialogRegistry.dialogTaskRegistry.get(daClass).size();
+            }
+            // add contribution from non-dialog tasks
+            for (Class<? extends NonDialogTask> taskClass : DialogRegistry.nonDialogTaskRegistry.get(daClass)){
+                NonDialogTaskPreferences preferences = taskClass.newInstance().getPreferences();
+                predictedRewardDifference += predictedConfidence * preferences.rewardForCorrectExecution;
+                predictedRewardDifference -= (1 - predictedConfidence) * preferences.penaltyForIncorrectExecution;
+                predictedRewardDifference -= currentConfidence * preferences.rewardForCorrectExecution;
+                predictedRewardDifference += (1 - currentConfidence) * preferences.penaltyForIncorrectExecution;
+                totalReward += currentConfidence * predictedRewardDifference /
+                        DialogRegistry.nonDialogTaskRegistry.get(daClass).size();
             }
         }
-
-
-
-        StringDistribution currentDialogActMarginal = DU.marginalSlotPathDistribution("dialogAct");
-        for (String dialogAct : currentDialogActMarginal.keySet()){
-            Class<? extends DialogAct> daClass = DialogRegistry.dialogActNameMap.get(dialogAct);
-            for (Class<? extends DialogTask> dialogTask : DialogRegistry.dialogTaskRegistry.get(daClass)){
-                totalReward += currentDialogActMarginal.get(dialogAct)*0;
-            }
-        }
-
         return totalReward;
     }
+
+
+
+
 
     public static Double predictConfidenceAfterJointGain(DiscourseUnit DU, Double factor, Double topJointConfidence){
         assert 0.0 <= factor && 1.0 >= factor;
--- yoda/src/main/java/edu/cmu/sv/task_interface/non_dialog_task/NonDialogTask.java
+++ yoda/src/main/java/edu/cmu/sv/task_interface/non_dialog_task/NonDialogTask.java
@@ -12,8 +12,14 @@ public interface NonDialogTask {
     // return preferences object
     public NonDialogTaskPreferences getPreferences();
 
+    // accessors for the DialogTask's taskSpec
+    public void setTaskSpec(SemanticsModel taskSpec);
+    public SemanticsModel getTaskSpec();
+    // a generic function to see if the important parameters for this task match up
+    public boolean meetsTaskSpec(SemanticsModel otherSpec);
+
     // interpret result as the probability that the taskSpec can be executed (must be 0-1)
-    public double assessExecutability(SemanticsModel taskSpec);
+    public double assessExecutability();
 
     // return the string identifier of the executing task (taskID)
     public String execute(SemanticsModel taskSpec);
