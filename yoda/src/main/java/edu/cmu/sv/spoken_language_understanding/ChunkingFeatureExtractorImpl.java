899b98930be1a4749025a751df557fda6bcd805e
David Cohen <david.cohen@west.cmu.edu>
2014-12-29 11:31:49 -0800

    added feature extraction implementations for chunking and classification

---
+++ yoda/src/main/java/edu/cmu/sv/spoken_language_understanding/ChunkingFeatureExtractorImpl.java
@@ -0,0 +1,49 @@
+package edu.cmu.sv.spoken_language_understanding;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.json.simple.JSONObject;
+
+import java.util.*;
+
+/**
+ * Created by David Cohen on 12/29/14.
+ */
+public class ChunkingFeatureExtractorImpl implements ChunkingFeatureExtractor{
+    static Map<String, Integer> contextFeaturePositionMap = new HashMap<>();
+
+    @Override
+    public Pair<List<Double>, List<List<String>>> generateFeatures(String inputString, JSONObject surroundingStructure, String contextPathInStructure) {
+
+        //// collect context features
+        Set<String> featuresPresent = new HashSet();
+        String[] fillerPath = contextPathInStructure.split("\\.");
+        for (int i = 0; i < fillerPath.length; i++) {
+            featuresPresent.add("NodeContext: " + (fillerPath.length - i) + ", " + fillerPath[i]);
+        }
+
+        //// assemble context feature vector
+        List<Double> contextFeatures = new LinkedList<>();
+        for (int i = 0; i < contextFeaturePositionMap.size(); i++) {
+            for (String presentFeature : featuresPresent) {
+                if (contextFeaturePositionMap.containsKey(presentFeature) && contextFeaturePositionMap.get(presentFeature).equals(i)){
+                    contextFeatures.add(1.0);
+                } else if (!contextFeaturePositionMap.containsKey(presentFeature)) {
+                    System.out.println("WARNING: present feature not in model: "+presentFeature);
+                } else {
+                    contextFeatures.add(0.0);
+                }
+            }
+        }
+
+        //// collect token feature vectors
+        List<List<String>> tokenFeatures = new LinkedList<>();
+        String[] tokens = inputString.split(" ");
+        for (int i = 0; i < tokens.length; i++) {
+            // the word is the only feature
+            tokenFeatures.add(new LinkedList<>(Arrays.asList(tokens[i])));
+        }
+
+        return new ImmutablePair<>(contextFeatures, tokenFeatures);
+    }
+}
---
+++ yoda/src/main/java/edu/cmu/sv/spoken_language_understanding/ClassificationFeatureExtractorImpl.java
@@ -0,0 +1,56 @@
+package edu.cmu.sv.spoken_language_understanding;
+
+import com.google.common.collect.Iterables;
+import org.json.simple.JSONObject;
+
+import java.util.*;
+
+/**
+ * Created by David Cohen on 12/29/14.
+ */
+public class ClassificationFeatureExtractorImpl implements ClassificationFeatureExtractor {
+    static Map<String, Integer> featurePositionMap = new HashMap<>();
+
+    @Override
+    public List<Double> generateFeatures(String inputString, JSONObject surroundingStructure, String contextPathInStructure) {
+        Set<String> featuresPresent = new HashSet<>();
+
+        //// collect features present
+        String[] tmp = inputString.split(" ");
+        List<String> tokens = new LinkedList<>(Arrays.asList(tmp));
+
+        // collect unigram features
+        for (int i = 0; i < tokens.size(); i++) {
+            featuresPresent.add("Unigram: "+tokens.get(i));
+        }
+
+        tokens.add(0, "<S>");
+        tokens.add("</S>");
+
+        // collect bigram features
+        for (int i = 0; i < tokens.size() - 1; i++) {
+            featuresPresent.add("Bigram: "+tokens.get(i)+", "+tokens.get(i+1));
+        }
+
+        // collect context features
+        String[] fillerPath = contextPathInStructure.split("\\.");
+        for (int i = 0; i < fillerPath.length; i++) {
+            featuresPresent.add("NodeContext: " + (fillerPath.length - i) + ", " + fillerPath[i]);
+        }
+
+        //// assemble feature vector
+        List<Double> features = new LinkedList<>();
+        for (int i = 0; i < featurePositionMap.size(); i++) {
+            for (String presentFeature : featuresPresent) {
+                if (featurePositionMap.containsKey(presentFeature) && featurePositionMap.get(presentFeature).equals(i)){
+                    features.add(1.0);
+                } else if (!featurePositionMap.containsKey(presentFeature)) {
+                    System.out.println("WARNING: present feature not in model: "+presentFeature);
+                } else {
+                    features.add(0.0);
+                }
+            }
+        }
+        return features;
+    }
+}
