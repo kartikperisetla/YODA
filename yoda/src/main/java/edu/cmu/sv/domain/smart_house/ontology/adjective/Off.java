4793b645a4d11347e39673763adfa8ba8de3cfc3
David Cohen <david.cohen@west.cmu.edu>
2015-03-05 15:57:41 -0800

    small pointless changes

--- yoda/src/main/java/edu/cmu/sv/domain/smart_house/ontology/adjective/Off.java
+++ yoda/src/main/java/edu/cmu/sv/domain/smart_house/ontology/adjective/Off.java
@@ -14,3 +14,5 @@ public class Off extends PowerStateAdjective {
         return 100;
     }
 }
+
+
--- yoda/src/main/java/edu/cmu/sv/domain/smart_house/ontology/noun/Appliance.java
+++ yoda/src/main/java/edu/cmu/sv/domain/smart_house/ontology/noun/Appliance.java
@@ -1,9 +1,9 @@
 package edu.cmu.sv.domain.smart_house.ontology.noun;
 
-import edu.cmu.sv.domain.yoda_skeleton.ontology.noun.Noun;
+import edu.cmu.sv.domain.yoda_skeleton.ontology.noun.PhysicalNoun;
 
 /**
- * Created by cohend on 3/4/15.
+ * Created by David Cohen on 3/4/15.
  */
-public class Appliance extends Noun {
+public class Appliance extends PhysicalNoun {
 }
