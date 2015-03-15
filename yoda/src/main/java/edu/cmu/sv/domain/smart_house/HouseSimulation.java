32b84165ee2f8475c157b73798fa36d049f970e8
Daniel Harbor <dan@Daniels-MBP.sv.cmu.local>
2015-03-14 17:16:42 -0700

    removed HouseSimulation file

--- yoda/src/main/java/edu/cmu/sv/domain/smart_house/HouseSimulation.java
+++
@@ -1,9 +0,0 @@
-package edu.cmu.sv.domain.smart_house;
-
-/**
- * Created by David Cohen on 3/7/15.
- */
-public class HouseSimulation {
-    public static String POI_0000_powerState = "off";
-    public static String POI_0001_powerState = "off";
-}
