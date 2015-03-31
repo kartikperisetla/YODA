31fdc65c3d4754d54429729a59f1114bd3da006e
David Cohen <david.cohen@west.cmu.edu>
2015-03-31 11:32:27 -0700

    lunch: SLU: adding a YODA skeleton SLU dataset to test domain-generic stuff - specifically time

---
+++ yoda/src/main/java/edu/cmu/sv/domain/yoda_skeleton/data/YodaSkeletonSLUDataset.java
@@ -0,0 +1,17 @@
+package edu.cmu.sv.domain.yoda_skeleton.data;
+
+import edu.cmu.sv.spoken_language_understanding.SLUDataset;
+
+/**
+ * Created by David Cohen on 3/11/15.
+ */
+public class YodaSkeletonSLUDataset extends SLUDataset {
+    public YodaSkeletonSLUDataset() {
+//"three thirty on tuesday"
+//"2:35"
+//"tomorrow"
+
+//        add(new ImmutablePair<>("is the air conditioner on",
+//                new SemanticsModel("{\"dialogAct\":\"YNQuestion\",\"verb\":{\"Agent\":{\"class\":\"AirConditioner\"},\"Patient\":{\"HasPowerState\":{\"class\":\"On\"},\"class\":\"UnknownThingWithRoles\"},\"class\":\"HasProperty\"}}")));
+    }
+}
