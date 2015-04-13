98b4af6b41f349e355247d2a42f0939e0ca9e108
Daniel Harbor <harbord2@gmail.com>
2015-04-13 14:59:45 -0700

    Added temperature attribute to GUIRoom and added new non-dialog task for increasing temperature

--- yoda/src/main/java/edu/cmu/sv/domain/smart_house/GUI/GUIRoom.java
+++ yoda/src/main/java/edu/cmu/sv/domain/smart_house/GUI/GUIRoom.java
@@ -5,10 +5,12 @@ import java.util.ArrayList;
 
 public class GUIRoom extends GUIThing {
 	private int dustLevel;
+	private double temperature;
 
-	public GUIRoom(String name, String correspondingURI, int dustLevel) {
+	public GUIRoom(String name, String correspondingURI, int dustLevel, double temperature) {
 		super(name, null, correspondingURI);
         this.dustLevel = dustLevel;
+		this.temperature = temperature;
 	}
 
     public int getDustLevel() {
@@ -19,16 +21,41 @@ public class GUIRoom extends GUIThing {
         this.dustLevel = dustLevel;
     }
 
+	public double getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(double temperature) {
+		this.temperature = temperature;
+	}
+
     @Override
 	public List<String> provideDetails() {
 		List<String> ans = new ArrayList<>();
 		ans.add(this.getName() + " entities: \n");
+		ans.add("Temp: " + String.valueOf(temperature) + (char)186 + "F\n");
 		for(GUIThing o : Simulator.getThings()) {
-			if(o.getRoom() == this.getRoom()) {  // check primitive comparison
+			if(!(o instanceof GUIRoom) && o.getRoom().equals(this.getRoom())) {
 				ans.add(o.getName() + "\n");
 			}
 		}
 		return ans;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if(this == obj)
+			return true;
+		if((obj == null) || (obj.getClass() != this.getClass()))
+			return false;
+		GUIRoom otherRoom = (GUIRoom)obj;
+		return (this.getName() == otherRoom.getName() ||
+				(this.getName() != null && this.getName().equals(otherRoom.getName()))) &&
+				(this.getName() == otherRoom.getName() ||
+				(this.getCorrespondingURI() != null && this.getCorrespondingURI().equals(otherRoom.getCorrespondingURI()))) &&
+				this.dustLevel == otherRoom.dustLevel &&
+				this.temperature == otherRoom.temperature;
 	}
 
 }
